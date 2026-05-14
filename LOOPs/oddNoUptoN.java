bb98735ba59ef1de87af4138da79894eef69ae45
MrCypar10 <arbaazakhtar06@gmail.com>
2026-05-14 22:05:37 +0530

    Add files via upload

---
+++ LOOPs/oddNoUptoN.java
@@ -0,0 +1,11 @@
+public class oddNoUptoN {
+public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int i = 1 ;
+    for(int i =1;i<=10;i++){
+        if(i%2==0)
+            continue;
+      System.out.println(i);
+    }
+}    
+}
