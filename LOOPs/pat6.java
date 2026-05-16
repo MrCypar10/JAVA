f881da6ee22198e2e6e5c65bb1caa2597fdcc583
MrCypar10 <arbaazakhtar06@gmail.com>
2026-05-16 17:03:10 +0000

    #

---
+++ LOOPs/pat6.java
@@ -0,0 +1,13 @@
+
+class pat6 {
+    public static void main(String[] args) {
+        int  n = 0;
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                n++;
+                System.out.print(n+" ");
+            }
+            System.out.println();
+        }
+    }
+}
