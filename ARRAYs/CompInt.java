8d1dbfbecf175fe738e8c1a57a9757fb1ccfb078
MrCypar10 <arbaazakhtar06@gmail.com>
2026-05-26 18:42:25 +0000

    #

--- ARRAYs/CompInt.java
+++ ARRAYs/CompInt.java
@@ -5,20 +5,18 @@ public class CompInt {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 10 Numbers : ");
-        int[] arr = new int[10];
+        System.out.println("Enter 10 Numbers ");
+        int arr[] = new int[10];
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Enter the Number to See if thatis present or Not");
+        System.out.println("Enter the Number");
         int n = sc.nextInt();
         for (int i = 0; i < 10; i++) {
-            if (n != arr[i]) {
-                System.out.println("The Number is Not Present");
-            } else if (n == arr[i]) {
-                System.out.println("No is present at : " + i);
+            if (n == arr[i]) {
+                System.out.println("The Number [" + n + "] is Present in Array at : " + i);
+                break;
             }
         }
-
     }
 }
