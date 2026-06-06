f42077fb09e1e1f3ee8dd6dec5b2f5f1d2a32a8b
MrCypar10 <arbaazakhtar06@gmail.com>
2026-06-06 07:13:53 +0000

    method Ex1

---
+++ OOPs/class_obj/methods/ex1.java
@@ -0,0 +1,15 @@
+
+class ex1 {
+
+    public static void greetings(String name) {
+        System.out.println("Hello " + name);
+        System.out.println("how are you ?");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        greetings("Deepak");
+        greetings("Amit");
+        greetings("Cypar");
+    }
+}
