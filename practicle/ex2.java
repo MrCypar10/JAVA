eafcb7ea1175dc4f31e6e770dbf3244d0b90c4ba
MrCypar10 <arbaazakhtar06@gmail.com>
2026-07-18 12:59:46 +0000

    practical day (loop) ex2.

---
+++ practicle/ex2.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class ex2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int a = sc.nextInt();
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
