88e95bc9bc170e83aef631cb4d8cca9ce92ae320
MrCypar10 <arbaazakhtar06@gmail.com>
2026-07-18 12:58:05 +0000

    ex1 of practical day(loops).

---
+++ practicle/ex1.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class ex1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int a = sc.nextInt();
+        for (int i = a; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+
+}
