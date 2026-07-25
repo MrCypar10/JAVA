27b244f412cdb59a116eaf65a531e55fd1778e89
MrCypar10 <arbaazakhtar06@gmail.com>
2026-07-25 13:03:05 +0000

    updated

--- practicle Loops/ex9_perfectNumber.java
+++ practicle Loops/ex9_perfectNumber.java
@@ -9,7 +9,7 @@ class ex9_perfectNumber {
         int n = sc.nextInt();
 
         int sum = 0;
-        for(int i = n-1; i>=1; i--){
+        for(int i = 1; i<n/2; i++){
             if(n%i==0){
                 sum+=i;
             }
