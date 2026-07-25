409d64eefe6572e8b54c9d23b3d034c6bfdac57c
MrCypar10 <arbaazakhtar06@gmail.com>
2026-07-25 13:30:34 +0000

    Prime Number

---
+++ practicle Loops/PrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PrimeNumber{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int count= 0;
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            if (n%i==0) {
+                count++;
+            }
+            //if(n%i==0){break;}
+            
+        }
+
+        //if(i==n){"prime number"} else {Not a prime number}
+        if (count==2) {
+            System.out.println(n+" : is a Prime Number");
+        }
+        else{
+            System.out.println(n+": is not a Prime Number");
+        }
+    }
+}
