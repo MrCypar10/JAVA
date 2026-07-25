91831d920781ce7ff849617680dcc8437fbcc8aa
MrCypar10 <arbaazakhtar06@gmail.com>
2026-07-25 13:14:05 +0000

    Harshad NUmber

--- practicle Loops/HarshadNumber.java
+++ practicle Loops/HarshadNumber.java
@@ -1,3 +1,25 @@
+import java.util.*;
 public class HarshadNumber {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        int m=0;
+        int org =n;
+        int sum =0;
+
+       while(n>0){
+            m=n%10;
+            sum+=m;
+
+            n=n/10;
+        }
+
+    if(org%sum==0){
+        System.out.println(org +" is Harsad Number");
+        }
+        else{
+            System.out.println(org+" Not a Harshad Number");
+        }
+    }
 }
