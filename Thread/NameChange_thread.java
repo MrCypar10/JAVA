5d6171897798bc7feb3c87d680fd48d9301e6aea
MrCypar10 <arbaazakhtar06@gmail.com>
2026-08-26 17:32:34 +0000

    Changing or seting Thread Name.

---
+++ Thread/NameChange_thread.java
@@ -0,0 +1,20 @@
+public class NameChange_thread {
+    public static void main(String[] args) {
+        Thread th = Thread.currentThread();
+        System.out.println("Before Thread Name Changed. (Default) : "+th );
+
+        th.setName("Cypar");
+        System.out.println("After Thread Name Changed. : "+th );
+        
+        try {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(th.getName()+" : " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+        System.out.println(th.getName() +" Interrupted !!");
+        }
+        System.out.println(th.getName()+"  Compeleted!!");
+    
+    }
+}
